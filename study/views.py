8e7f415235bce51c1e0dfa55e17ac388f64400df
David Waterson <dwaterson68@gmail.com>
2021-04-23 18:37:17 +0200

    initial commit for study project

---
+++ study/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+gems = [
+    {
+        'book': 'Matthew',
+        'chapter': 24,
+        'summary': 'the end is coming',
+        'date_entered': 'April 20, 2019'         
+    },
+    {
+        'book': '1 Corinthians',
+        'chapter': 7,
+        'summary': 'singleness and marriage',
+        'date_entered': 'March 1, 2020'         
+    },]
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'gems': gems
+    }
+    return render(request, 'study/home.html', context)
+
+
+def about(request):
+    return render(request, 'study/about.html')
